02414001077aae857eb94bd11dfd665e2d809ad7
chenguangsunnychan <chenguangsunnychan@didichuxing.com>
2018-10-07 16:11:00 +0800

    run

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,6 @@
 
 SUNNYTEST_HOME=$(cd "$(dirname $0)/.."; pwd)
 MAIN_CTRL_FILE=${SUNNYTEST_HOME}'/main/MainCtrl.php'
-alias php=${SUNNYTEST_HOME}'/php/bin/php'
+# alias php=${SUNNYTEST_HOME}'/php/bin/php'
 
 php ${MAIN_CTRL_FILE} $@
